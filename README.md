f2a95329c2f9d840199fad6384485a734cd2d937
jose.padin <jose.mendez@kubicum.com>
2021-12-03 11:34:37 +0100

    Improvements
    * Updated README.md
    * Improved the code to support different date format inputs
    * Updated the html

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 ### This is a little project made for FreeCodeCamp, in the `Back end development and APIs` module
 
 - Relevant items used in this project:
-[NODE, NPM, Express]
+[NODE, NPM, Express, Nodemon]
--- server.js
+++ server.js
@@ -10,21 +10,35 @@ app.get('/', (req, res) => {
 	res.sendFile(path.join(__dirname, 'views/index.html'));
 })
 
-app.get('/:date', (req, res) => {
-	parameters = req.params
+app.get('/api/', (req, res) => {
+	_date = new Date();
+	return res.status(200).json({utc: _date, unix: Date.parse(_date)})
+});
 
-	if (parameters.date !== "favicon.ico") {
-		try {
-			_date_parsed = parameters.date.split('-')
-			month = _date_parsed[1] - 1
-			_date = new Date(_date_parsed[2], month, _date_parsed[0])
-			return res.status(200).json({date: _date, timestamp: Date.parse(_date)});
-		} catch (error) {
-			console.log(error);
-			return res.status(400).json({result: "Wrong date format"})
+app.get('/api/:date', (req, res) => {
+	if (req.params.date !== "favicon.ico") {
+		date = req.params.date;
+
+		if (date.indexOf('-') !== -1) {
+			try {
+				_date_parsed = date.split('-');
+				month = _date_parsed[1] - 1;
+				_date = new Date(_date_parsed[2], month, _date_parsed[0]);
+				if (_date == 'Invalid Date') {
+					return res.status(400).json({error: 'Invalid Date'});
+				}
+				return res.status(200).json({utc: _date, unix: Date.parse(_date)});
+			} catch (error) {
+				console.log(error);
+				return res.status(400).json({result: 'Wrong date format'});
+			}
+		} else {
+			_date = new Date(date);
+			return res.status(200).json({utc: _date, unix: Date.parse(_date)});
 		}
 	}
-	return res.status(400).json({result: "Wrong date format"})
+
+	return res.status(400).json({result: 'Wrong date format'});
 })
 
 
