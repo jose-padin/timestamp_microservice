b4db808ee4908cb2ad79d93f76e4f89aa35a9fe2
jose.padin <jose.mendez@kubicum.com>
2021-12-03 15:07:30 +0100

    Forgot save...

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC
+const cors = require("cors");
+app.use(cors({ optionSuccessStatus: 200 })); // some legacy browsers choke on 204
+
 const port = process.env.PORT || 3005;
 
 app.get('/', (req, res) => {
